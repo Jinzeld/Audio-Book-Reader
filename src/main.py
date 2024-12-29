84073a2aec4a0fdb9812cd45bd8ae4ef81162ab3
Jinhui Zhen <123280886+Jinzeld@users.noreply.github.com>
2024-12-29 13:23:48 -0800

    added tkinter and pdf reader functionality

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import pyttsx3
 import PyPDF2
-from tkinter.filedialog import *
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+# Initialize Tk root window and hide it
+root = Tk()
 
 
+book = askopenfilename()
+pdfreader = PyPDF2.pdfFileReader(book)
+pages = pdfreader.numPages
 
-
-# def read_pdf(file_path):
-#     pdf_reader = PyPDF2.PdfReader(file_path)
-#     text = ""
-#     for page in pdf_reader.pages:
-#         text += page.extract_text()
-#     return text
+for num in range(0, pages):
+    page = pdfreader.getPage(num)
+    text = page.extractText()
+    player = pyttsx3.init()
+    player.runAndWait()
 
