e9a4aef5cab3a563f796eda2ae205cc3a6722516
Jinhui Zhen <123280886+Jinzeld@users.noreply.github.com>
2024-12-28 22:03:48 -0800

    file upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyttsx3
+import PyPDF2
+from tkinter.filedialog import *
+
+
+
+
+# def read_pdf(file_path):
+#     pdf_reader = PyPDF2.PdfReader(file_path)
+#     text = ""
+#     for page in pdf_reader.pages:
+#         text += page.extract_text()
+#     return text
+
